553b3111b9860045c4a6cb604c47f0db136f3060
y-hyun2 <hyma0214@gmail.com>
2024-04-13 22:49:53 +0900

    upload session_0411

---
+++ todolist/urls.py
@@ -0,0 +1,27 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.welcome, name='welcome'),  # welcome.html 페이지 매핑
+    path('work/', views.work, name='work'),   # work.html 페이지 매핑
+    path('work/<int:todo_id>/', views.work, name='work_detail'),   # work.html 조회
+]
+
+urlpatterns = [
+    path('', views.welcome, name='welcome'),  
+    path('work/', views.work, name='work'), 
+    path('work/<int:todo_id>/', views.work, name='work_detail'),
+    
+    # update url 추가   
+    path('update/<int:todo_id>/', views.update, name='update'),
+]
+
+urlpatterns = [
+    path('', views.welcome, name='welcome'),  
+    path('work/', views.work, name='work'), 
+    path('work/<int:todo_id>/', views.work, name='work_detail'),
+    path('update/<int:todo_id>/', views.update, name='update'),
+    
+    # delete url 추가   
+	  path('delete/<int:todo_id>/', views.delete, name='delete'),
+]
